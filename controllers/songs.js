54553d0b397d5fc0668b3dc19aa8be8d42dcdec0
amymin00 <amy.min7117@gmail.com>
2022-04-28 09:14:35 -0400

    clean up songs list functionality

--- controllers/songs.js
+++ controllers/songs.js
@@ -12,7 +12,18 @@ const findSongs = async (req, res) => {
     res.json(data);
 };
 
+const findSongsById = async (req, res) => {
+    const ids = req.body.songs;
+    const songs = [];
+    await Promise.all(ids.map(async id => {
+        const songData = await findSong(id);
+        songs.push(songData);
+    }));
+    res.json(songs);
+}
+
 export default app => {
-    app.get('/api/songs/:id', findSongById);
+    app.get('/api/songs/id/:id', findSongById);
     app.get('/api/songs/search/:query', findSongs);
+    app.post('/api/songs/list', findSongsById);
 };
--- database/posts/postsDao.js
+++ database/posts/postsDao.js
@@ -1,7 +1,7 @@
 import postsModel from './postsModel.js';
 
 export const findAllPosts = () => postsModel.find();
-export const findPost = id => postsModel.findOne({_id: id});
+export const findPost = id => postsModel.findById(id);
 export const findPostsByAuthor = id => postsModel.find({author: id});
 export const createPost = post => postsModel.create(post);
 export const deletePost = id => postsModel.deleteOne({_id: id});
