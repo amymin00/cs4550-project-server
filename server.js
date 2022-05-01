ecfc3e72bf2e748ce2abe02a5029901383cdebe5
amymin00 <amy.min7117@gmail.com>
2022-05-01 15:17:25 -0400

    add netlify origin link

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.use(cors({
         'http://localhost:4000', 
         'http://api.spotify.com',
         'http://guileless-rabanadas-1bfb30.netlify.app',
+        'http://main--guileless-rabanadas-1bfb30.netlify.app/',
         'http://cs4550-proj-server.herokuapp.com/'
     ]
 }));
